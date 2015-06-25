8fe94da5f7db79149d8828a50f790b530fc2f441
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-06-25 12:16:00 -0300

    Update do gson-hypermedia, jpa-serializer to 0.2.0

--- src/main/java/br/eti/clairton/vraptor/crud/serializer/ModelDeserializer.java
+++ src/main/java/br/eti/clairton/vraptor/crud/serializer/ModelDeserializer.java
@@ -8,12 +8,7 @@ import javax.persistence.EntityManager;
 import javax.validation.constraints.NotNull;
 
 import net.vidageek.mirror.dsl.Mirror;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import br.eti.clairton.jpa.serializer.JpaDeserializer;
-import br.eti.clairton.jpa.serializer.JpaSerializer;
 import br.eti.clairton.repository.Model;
 
 import com.google.gson.JsonDeserializationContext;
@@ -23,13 +18,12 @@ import com.google.gson.JsonParseException;
 
 /**
  * Serializa os objetos da de forma a integrar com o modo ActiveSerializer.
- * 
+ *
  * @author Clairton Rodrigo Heinzen<clairton.rodrigo@gmail.com>
  */
 @Vetoed
 public class ModelDeserializer implements JsonDeserializer<Model> {
 	private final JpaDeserializer<Model> delegate;
-	private final Logger logger = LogManager.getLogger(JpaSerializer.class);
 
 	@Deprecated
 	protected ModelDeserializer() {
@@ -39,8 +33,7 @@ public class ModelDeserializer implements JsonDeserializer<Model> {
 	@Inject
 	public ModelDeserializer(@NotNull final EntityManager entityManager,
 			@NotNull final Mirror mirror) {
-		delegate = new JpaDeserializer<Model>(entityManager, mirror, logger) {
-		};
+		delegate = new JpaDeserializer<Model>(entityManager);
 	}
 
 	@Override
--- src/main/java/br/eti/clairton/vraptor/crud/serializer/ModelSerializer.java
+++ src/main/java/br/eti/clairton/vraptor/crud/serializer/ModelSerializer.java
@@ -7,10 +7,6 @@ import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 
 import net.vidageek.mirror.dsl.Mirror;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import br.eti.clairton.jpa.serializer.JpaSerializer;
 import br.eti.clairton.repository.Model;
 
@@ -20,18 +16,16 @@ import com.google.gson.JsonSerializer;
 
 /**
  * Deserializa os objetos da de forma a integrar com o modo ActiveSerializer.
- * 
+ *
  * @author Clairton Rodrigo Heinzen<clairton.rodrigo@gmail.com>
  */
 @Vetoed
 public class ModelSerializer implements JsonSerializer<Model> {
-	private final Logger logger = LogManager.getLogger(JpaSerializer.class);
 	private final JpaSerializer<Model> jpaSerializer;
 
 	@Inject
 	public ModelSerializer(final Mirror mirror) {
-		jpaSerializer = new JpaSerializer<Model>(mirror, logger) {
-		};
+		jpaSerializer = new JpaSerializer<Model>();
 	}
 
 	public void addIgnoredField(@NotNull final String field) {
