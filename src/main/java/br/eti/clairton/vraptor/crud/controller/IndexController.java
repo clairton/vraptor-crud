475baab95a86dbc4264d1441b4a19dd42ab97b44
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-02-05 13:02:31 -0200

    IndexController#index forward to index.html

---
+++ src/main/java/br/eti/clairton/vraptor/crud/controller/IndexController.java
@@ -0,0 +1,30 @@
+package br.eti.clairton.vraptor.crud.controller;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Result;
+
+@Controller
+@Path("/")
+public class IndexController {
+	private final Result result;
+	
+	public IndexController() {
+		this(null);
+	}
+	
+	@Inject
+	public IndexController(final Result result) {
+		super();
+		this.result = result;
+	}
+
+	@Get
+	public void index(){
+		result.forwardTo("index.html");
+	}
+	
+}
--- src/main/java/br/eti/clairton/vraptor/crud/controller/SinglePagePathResolver.java
+++ src/main/java/br/eti/clairton/vraptor/crud/controller/SinglePagePathResolver.java
@@ -3,7 +3,6 @@ package br.eti.clairton.vraptor.crud.controller;
 import javax.enterprise.inject.Specializes;
 import javax.inject.Inject;
 
-import br.com.caelum.vraptor.controller.ControllerMethod;
 import br.com.caelum.vraptor.http.FormatResolver;
 import br.com.caelum.vraptor.view.DefaultPathResolver;
 
@@ -25,12 +24,12 @@ public class SinglePagePathResolver extends DefaultPathResolver {
 	public SinglePagePathResolver(final FormatResolver resolver) {
 		super(resolver);
 	}
-
-	/**
-	 * Sempre deve retorna "index.html", pq em uma SPA não há navegação.
-	 */
-	@Override
-	public String pathFor(final ControllerMethod method) {
-		return "index.html";
-	}
+	
+	protected String getPrefix() {
+        return "/WEB-INF";
+    }
+	
+	protected String getExtension() {
+        return "html";
+    }
 }
